abcf088d09a71716bfdf0bc1a50983a2abb2a48c
Aubre Body <bodyaubre@gmail.com>
2021-04-25 22:30:46 +0000

    added a output.tf also added content to two files

--- ec2_instances.tf
+++ ec2_instances.tf
@@ -24,7 +24,40 @@ resource "aws_key_pair" "worker-key" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
+#Creating EC2 in us-east-1
+resource "aws_instance" "docker-master" {
+  provider                    = aws.region-master
+  ami                         = data.aws_ssm_parameter.linuxAmiMaster.value
+  instance_type               = var.instance-type
+  key_name                    = aws_key_pair.master-key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.jenkins-sg.id]
+  subnet_id                   = aws_subnet.subnet_1_master.id
 
+  tags = {
+    Name = "docker_master_tf"
+  }
+
+  depends_on = [aws_main_route_table_association.set-master-default-rt-assoc]
+}
+
+
+#Create EC2 in us-west-2
+resource "aws_instance" "docker-worker" {
+  provider                    = aws.region-worker
+  count                       = var.workers-count
+  ami                         = data.aws_ssm_parameter.linuxAmiWork.value
+  instance_type               = var.instance-type
+  key_name                    = aws_key_pair.worker-key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.jenkins-sg-worker.id]
+  subnet_id                   = aws_subnet.subnet_2_master.id
+
+  tags = {
+    Name = join("_", ["docker_worker_tf", count.index + 1])
+  }
+  depends_on = [aws_main_route_table_association.set-worker-default-rt-assoc, aws_instance.docker-master]
+}
 
 
 
